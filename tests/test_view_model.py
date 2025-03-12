cad58c3020b574005989dbe5354f6799f4e8479c
Giorgio Sironi <work@giorgiosironi.com>
2025-03-12 15:10:25 +0000

    Expand tests, while extracting common assertions

--- tests/test_view_model.py
+++ tests/test_view_model.py
@@ -1,4 +1,10 @@
+from dataclasses import dataclass
 from view_model import construct_view_model
+
+@dataclass
+class AliveCell:
+    x: int
+    y: int
 
 max_x = 2
 max_y = 2
@@ -8,12 +14,25 @@ total_columns = max_x + 1
 def count_alive_cells(view_model):
     return sum(cell for row in view_model for cell in row)
 
+def assert_rows(view_model):
+    assert len(view_model) == total_rows, f"Expected {total_rows} rows"
+
+def assert_columns(view_model):
+    for row_index, row in enumerate(view_model):
+        assert len(row) == total_columns, f"Expected {total_columns} cells in row {row_index}"
+
 def test_empty_list_of_alive_cells_displays_all_dead_cells():
     alive_cells = []
     view_model = construct_view_model(alive_cells, max_x=max_x, max_y=max_y)
     
-    assert len(view_model) == total_rows, f"Expected {total_rows} rows"
-    for row_index, row in enumerate(view_model):
-        assert len(row) == total_columns, f"Expected {total_columns} cells in row {row_index}"
+    assert_rows(view_model)
+    assert_columns(view_model)
     assert count_alive_cells(view_model) == 0, "Expect all cells to be dead"
 
+def test_one_alive_cells_is_displayed():
+    alive_cells = [AliveCell(1, 2)]
+    view_model = construct_view_model(alive_cells, max_x=max_x, max_y=max_y)
+    
+    assert_rows(view_model)
+    assert_columns(view_model)
+    #assert count_alive_cells(view_model) == 1, "Expect one cells to be alive"
