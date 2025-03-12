45bed60f04a614f1c1409d52fa4228b96add4253
Giorgio Sironi <work@giorgiosironi.com>
2025-03-12 13:41:18 +0000

    Extract reusable test setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,15 @@
 import pytest
+from bs4 import BeautifulSoup
 from app import app
 
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
-    return app.test_client() 
+    return app.test_client()
+
+@pytest.fixture
+def page(client):
+    def _get_page(url):
+        response = client.get(url)
+        return BeautifulSoup(response.data, 'html.parser')
+    return _get_page 
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,17 +1,13 @@
 from bs4 import BeautifulSoup
 
-def test_home_page(client):
+def test_home_page(page):
     """Test that home page loads and contains expected h1"""
-    response = client.get('/')
-    assert response.status_code == 200
-    assert b'<h1>Hello, world!</h1>' in response.data
+    soup = page('/')
+    assert soup.select_one('h1').text == 'Hello, world!'
 
-def test_blinker_page(client):
+def test_blinker_page(page):
     """Test that blinker example page loads"""
     width, height = 8, 6
-    response = client.get('/examples/blinker')
-    assert response.status_code == 200
-    
-    soup = BeautifulSoup(response.data, 'html.parser')
+    soup = page('/examples/blinker')
     assert len(soup.select('td')) == width * height, f"Expecting a {width}x{height} table"
     assert len(soup.select('td.cell--alive')) == 3, "Expecting exactly 3 living cells"
