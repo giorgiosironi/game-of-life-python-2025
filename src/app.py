969e784ec6f529d7f65acd10f9283cc819317c8f
Giorgio Sironi <work@giorgiosironi.com>
2025-03-13 10:17:08 +0000

    Create a seam for an evolve  testable function

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from view_model import AliveCell, construct_view_model
 
 app = Flask(__name__, template_folder='../templates')
@@ -7,16 +7,21 @@ app = Flask(__name__, template_folder='../templates')
 def hello():
     return render_template('index.html')
 
-def _blinker_view_model():
-    width, height = 8, 6
-    alive_cells = [
-        AliveCell(2, 3),
-        AliveCell(3, 3),
-        AliveCell(4, 3),
-    ]
-    return construct_view_model(alive_cells, max_x=width-1, max_y=height-1)
+world_state_containing_a_blinker = [
+    AliveCell(2, 3),
+    AliveCell(3, 3),
+    AliveCell(4, 3),
+]
+
+def evolve(model):
+    return model
 
 @app.route('/examples/blinker')
 def blinker_example():
-    displayed_cells = _blinker_view_model()
+    generation = request.args.get('generation', '1')
+    width, height = 8, 6
+    world_state = world_state_containing_a_blinker
+    if generation == 2:
+        world_state = evolve(world_state)
+    displayed_cells = construct_view_model(world_state, max_x=width-1, max_y=height-1)
     return render_template('generation.html', displayed_cells=displayed_cells, title="Blinker Example")
