c993b242a0e9da07c33208e82e8997a016ecb66c
Giorgio Sironi <work@giorgiosironi.com>
2025-03-13 10:50:44 +0000

    Create a seam to count alive neighbors of all alive cells

--- src/world.py
+++ src/world.py
@@ -3,5 +3,10 @@ from alive_cell import AliveCell
 
 WorldState: TypeAlias = List[AliveCell]
 
+def count_alive_neighbors(world_state: WorldState, cell: AliveCell) -> int:
+    pass
+
 def evolve(world_state: WorldState) -> WorldState:
+    for cell in world_state:
+        alive_neighbors = count_alive_neighbors(world_state, cell)
     return []
