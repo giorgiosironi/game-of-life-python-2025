159da77d4bd4a1a53870c606cb21bd1a5f9e798b
Giorgio Sironi <work@giorgiosironi.com>
2025-03-13 10:25:07 +0000

    Document evolve's signature with type hints

--- src/world.py
+++ src/world.py
@@ -1,2 +1,7 @@
-def evolve(world_state):
+from typing import List, TypeAlias
+from alive_cell import AliveCell
+
+WorldState: TypeAlias = List[AliveCell]
+
+def evolve(world_state: WorldState) -> WorldState:
     return world_state 
