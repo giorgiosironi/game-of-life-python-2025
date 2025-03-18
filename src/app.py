1088dfdaf6a353ec09eea2f60200f8127ecb24af
Giorgio Sironi <work@giorgiosironi.com>
2025-03-18 14:59:54 +0000

    Allow customization of width and height of the window

--- src/app.py
+++ src/app.py
@@ -23,15 +23,16 @@ def evolve_to_generation(world_state, generation):
 @app.route('/examples/blinker')
 def blinker_example():
     generation = int(request.args.get('generation', '1'))
-    width, height = 8, 6
+    width = int(request.args.get('width', '8'))
+    height = int(request.args.get('height', '6'))
     world_state = evolve_to_generation(world_state_containing_a_blinker, generation)
         
     displayed_cells = construct_view_model(world_state, max_x=width-1, max_y=height-1)
-    next_state_path = f'/examples/blinker?generation={generation + 1}'
+    next_state_path = f'/examples/blinker?generation={generation + 1}&width={width}&height={height}'
     
     return render_template(
         'generation.html', 
         displayed_cells=displayed_cells, 
         title="Blinker Example",
-        next_state_path=next_state_path
+        next_state_path=next_state_path,
     )
