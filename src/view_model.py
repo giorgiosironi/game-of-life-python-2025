54cea39f00b6abf94cf13058a785e19fb6b78279
Giorgio Sironi <work@giorgiosironi.com>
2025-03-18 15:43:03 +0000

    Mechanically introduce type annotations in view_model module

--- src/view_model.py
+++ src/view_model.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-from world import AliveCell
+from world import AliveCell, WorldState
+from typing import List
 
 @dataclass
 class DisplayedCell:
@@ -7,12 +8,12 @@ class DisplayedCell:
     y: int
     alive: bool
 
-def construct_view_model(alive_cells, max_x, max_y):
-    return [_construct_row(alive_cells, max_x + 1, y) for y in range(max_y+1)]
+def construct_view_model(world_state: WorldState, max_x: int, max_y: int) -> List[List[DisplayedCell]]:
+    return [_construct_row(world_state, max_x + 1, y) for y in range(max_y+1)]
 
-def _construct_row(alive_cells, row_length, y):
-    return [DisplayedCell(x=x, y=y, alive=_is_cell_alive(alive_cells, x, y)) 
+def _construct_row(world_state: WorldState, row_length: int, y: int) -> List[DisplayedCell]:
+    return [DisplayedCell(x=x, y=y, alive=_is_cell_alive(world_state, x, y)) 
             for x in range(row_length)]
 
-def _is_cell_alive(alive_cells, x, y):
-    return AliveCell(x, y) in alive_cells
+def _is_cell_alive(world_state: WorldState, x: int, y: int) -> bool:
+    return AliveCell(x, y) in world_state
