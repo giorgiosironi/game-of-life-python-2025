18b2b09ea46b26b4683a3c6d1e9976ada12a0ea9
Giorgio Sironi <work@giorgiosironi.com>
2025-03-18 14:03:33 +0000

    Implement last rule via a `set` of considered cells

--- src/evolution.py
+++ src/evolution.py
@@ -1,11 +1,22 @@
 from neighborhood import find_neighbors
-from world import WorldState
+from world import WorldState, AliveCell
 
 def evolve(world_state: WorldState) -> WorldState:
     next_state = []
+    considered_cells = set()
+    
     for cell in world_state:
+        considered_cells.add(cell)
+        for neighbor in find_neighbors(cell):
+            considered_cells.add(neighbor)
+            
+    for cell in considered_cells:
         neighbors = find_neighbors(cell)
         alive_neighbors = sum(1 for neighbor in neighbors if neighbor in world_state)
-        if alive_neighbors == 2 or alive_neighbors == 3:
-            next_state.append(cell)
+        if cell in world_state:
+            if alive_neighbors == 2 or alive_neighbors == 3:
+                next_state.append(cell)
+        else:
+            if alive_neighbors == 3:
+                next_state.append(cell)
     return next_state
