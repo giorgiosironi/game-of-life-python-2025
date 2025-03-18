4a893248836ed6b7f1e1290e054c69314075e72f
Giorgio Sironi <work@giorgiosironi.com>
2025-03-18 16:24:41 +0000

    Add more naive type annotations

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,23 +2,23 @@ from bs4 import BeautifulSoup
 
 default_width, default_height = 8, 6
 
-def assert_table_has_custom_dimensions(soup, custom_width, custom_height):
+def assert_table_has_custom_dimensions(soup, custom_width, custom_height) -> None:
     """Checks for specific table dimensions"""
     assert len(soup.select('td')) == custom_width * custom_height, f"Expecting a {custom_width}x{custom_height} table"
 
-def assert_table_is_displayed(soup):
+def assert_table_is_displayed(soup) -> None:
     """Checks consistent and correct dimensions"""
     assert_table_has_custom_dimensions(soup, default_width, default_height)
 
-def assert_only_a_blinker_is_present(soup):
+def assert_only_a_blinker_is_present(soup) -> None:
     """Smoke tests that only 3 living cells are displayed"""
     assert len(soup.select('td.cell--alive')) == 3, "Expecting exactly 3 living cells"
 
-def assert_blinker_is_horizontal(soup):
+def assert_blinker_is_horizontal(soup) -> None:
     alive_y_coords = [int(cell['data-y']) for cell in soup.select('td.cell--alive')]
     assert len(set(alive_y_coords)) == 1, "Expecting all cells to share the same y coordinate"
 
-def assert_blinker_is_vertical(soup):
+def assert_blinker_is_vertical(soup) -> None:
     alive_x_coords = [int(cell['data-x']) for cell in soup.select('td.cell--alive')]
     assert len(set(alive_x_coords)) == 1, "Expecting all cells to share the same x coordinate"
 
@@ -26,31 +26,31 @@ def test_home_page_loads(page):
     soup = page('/')
     assert soup.select_one('h1').text == 'Hello, world!'
 
-def test_blinker_starts_horizontal(page):
+def test_blinker_starts_horizontal(page) -> None:
     soup = page('/examples/blinker')
     assert_table_is_displayed(soup)
     assert_only_a_blinker_is_present(soup)
     assert_blinker_is_horizontal(soup)
 
-def test_blinker_evolves_to_vertical(page):
+def test_blinker_evolves_to_vertical(page) -> None:
     soup = page('/examples/blinker?generation=2')
     assert_table_is_displayed(soup)
     assert_only_a_blinker_is_present(soup)
     assert_blinker_is_vertical(soup)
 
-def test_blinker_cycles_back_to_horizontal_in_any_odd_generation(page):
+def test_blinker_cycles_back_to_horizontal_in_any_odd_generation(page) -> None:
     soup = page('/examples/blinker?generation=3')
     assert_table_is_displayed(soup)
     assert_only_a_blinker_is_present(soup)
     assert_blinker_is_horizontal(soup)
     
-def test_blinker_cycles_back_to_vertical_in_any_even_generation(page):
+def test_blinker_cycles_back_to_vertical_in_any_even_generation(page) -> None:
     soup = page('/examples/blinker?generation=4')
     assert_table_is_displayed(soup)
     assert_only_a_blinker_is_present(soup)
     assert_blinker_is_vertical(soup)
 
-def test_the_world_window_can_have_custom_dimensions(page):
+def test_the_world_window_can_have_custom_dimensions(page) -> None:
     custom_width, custom_height = 4, 3
     soup = page(f'/examples/blinker?width={custom_width}&height={custom_height}')
     assert_table_has_custom_dimensions(soup, custom_width, custom_height)
--- tests/test_evolution.py
+++ tests/test_evolution.py
@@ -1,21 +1,21 @@
 from evolution import WorldState, evolve
 from world import AliveCell
 
-def test_empty_world_stays_empty():
+def test_empty_world_stays_empty() -> None:
     world_state: WorldState = []
     
     next_state = evolve(world_state)
     
     assert len(next_state) == 0, "Expected no cells to be alive in next generation"
 
-def test_underpopulation_single_alive_cell_dies():
+def test_underpopulation_single_alive_cell_dies() -> None:
     world_state: WorldState = [AliveCell(x=1, y=1)]
     
     next_state = evolve(world_state)
     
     assert AliveCell(1, 1) not in next_state, "Expected cell at (1,1) to die from underpopulation"
 
-def test_underpopulation_alive_cell_with_one_alive_neighbor_dies():
+def test_underpopulation_alive_cell_with_one_alive_neighbor_dies() -> None:
     world_state: WorldState = [
         AliveCell(x=1, y=1),
         AliveCell(x=1, y=2),
@@ -25,7 +25,7 @@ def test_underpopulation_alive_cell_with_one_alive_neighbor_dies():
     
     assert AliveCell(1, 1) not in next_state, "Expected cell at (1,1) to die from underpopulation"
 
-def test_survival_alive_cell_with_two_alive_neighbors_lives():
+def test_survival_alive_cell_with_two_alive_neighbors_lives() -> None:
     world_state: WorldState = [
         AliveCell(x=1, y=1),
         AliveCell(x=1, y=2),
@@ -36,7 +36,7 @@ def test_survival_alive_cell_with_two_alive_neighbors_lives():
     
     assert AliveCell(1, 1) in next_state, "Expected cell at (1,1) to survive with two neighbors" 
 
-def test_cell_with_three_neighbors_survives():
+def test_cell_with_three_neighbors_survives() -> None:
     cell = AliveCell(x=1, y=1)
     neighbors = [
         AliveCell(x=0, y=0),
@@ -49,7 +49,7 @@ def test_cell_with_three_neighbors_survives():
     
     assert cell in next_state, "Expected cell with three neighbors should survive" 
 
-def test_overcrowding_alive_cell_with_four_neighbors_dies():
+def test_overcrowding_alive_cell_with_four_neighbors_dies() -> None:
     cell = AliveCell(x=1, y=1)
     neighbors = [
         AliveCell(x=0, y=0),
@@ -63,7 +63,7 @@ def test_overcrowding_alive_cell_with_four_neighbors_dies():
     
     assert cell not in next_state, "Expected cell with four neighbors to die from overcrowding"
 
-def test_reproduction_dead_cell_with_three_neighbors_becomes_alive():
+def test_reproduction_dead_cell_with_three_neighbors_becomes_alive() -> None:
     world_state = [
         AliveCell(x=0, y=0),
         AliveCell(x=0, y=1),
