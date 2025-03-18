a1d0992fa5b6d3ff0ff839c42c5f88d275593c13
Giorgio Sironi <work@giorgiosironi.com>
2025-03-18 14:07:20 +0000

    Add an accidentally passing test for odd generations of a blinker

--- tests/test_app.py
+++ tests/test_app.py
@@ -24,3 +24,14 @@ def test_blinker_evolves(page):
     
     alive_x_coords = [int(cell['data-x']) for cell in soup.select('td.cell--alive')]
     assert len(set(alive_x_coords)) == 1, "Expecting all cells to share the same x coordinate"
+
+def test_blinker_cycles_back_in_any_odd_generation(page):
+    """Test that blinker example page evolves to the next generation"""
+    width, height = 8, 6
+    soup = page('/examples/blinker?generation=7')
+    assert len(soup.select('td')) == width * height, f"Expecting a {width}x{height} table"
+    assert len(soup.select('td.cell--alive')) == 3, "Expecting exactly 3 living cells"
+    
+    alive_y_coords = [int(cell['data-y']) for cell in soup.select('td.cell--alive')]
+    assert len(set(alive_y_coords)) == 1, "Expecting all cells to share the same y coordinate"
+
