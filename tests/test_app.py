67ea9f1143b817049e584abcb210fb4f7a04947d
Giorgio Sironi <work@giorgiosironi.com>
2025-03-18 14:15:37 +0000

    Extract custom assertions and document their power

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,37 +1,43 @@
 from bs4 import BeautifulSoup
 
-def test_home_page(page):
-    """Test that home page loads and contains expected h1"""
+width, height = 8, 6
+
+def assert_table_is_displayed(soup):
+    """Checks consistent and correct dimensions"""
+    assert len(soup.select('td')) == width * height, f"Expecting a {width}x{height} table"
+
+def assert_only_a_blinker_is_present(soup):
+    """Smoke tests that only 3 living cells are displayed"""
+    assert len(soup.select('td.cell--alive')) == 3, "Expecting exactly 3 living cells"
+
+def assert_blinker_is_horizontal(soup):
+    alive_y_coords = [int(cell['data-y']) for cell in soup.select('td.cell--alive')]
+    assert len(set(alive_y_coords)) == 1, "Expecting all cells to share the same y coordinate"
+
+def assert_blinker_is_vertical(soup):
+    alive_x_coords = [int(cell['data-x']) for cell in soup.select('td.cell--alive')]
+    assert len(set(alive_x_coords)) == 1, "Expecting all cells to share the same x coordinate"
+
+def test_home_page_loads(page):
     soup = page('/')
     assert soup.select_one('h1').text == 'Hello, world!'
 
 def test_blinker_starts_horizontal(page):
-    """Test that blinker starts in horizontal position (3 cells in same row)"""
-    width, height = 8, 6
     soup = page('/examples/blinker')
-    assert len(soup.select('td')) == width * height, f"Expecting a {width}x{height} table"
-    assert len(soup.select('td.cell--alive')) == 3, "Expecting exactly 3 living cells"
-    
-    alive_y_coords = [int(cell['data-y']) for cell in soup.select('td.cell--alive')]
-    assert len(set(alive_y_coords)) == 1, "Expecting all cells to share the same y coordinate"
+    assert_table_is_displayed(soup)
+    assert_only_a_blinker_is_present(soup)
+    assert_blinker_is_horizontal(soup)
 
-def test_blinker_evolves(page):
-    """Test that blinker example page evolves to the next generation"""
-    width, height = 8, 6
+def test_blinker_evolves_to_vertical(page):
     soup = page('/examples/blinker?generation=2')
-    assert len(soup.select('td')) == width * height, f"Expecting a {width}x{height} table"
-    assert len(soup.select('td.cell--alive')) == 3, "Expecting exactly 3 living cells"
-    
-    alive_x_coords = [int(cell['data-x']) for cell in soup.select('td.cell--alive')]
-    assert len(set(alive_x_coords)) == 1, "Expecting all cells to share the same x coordinate"
+    assert_table_is_displayed(soup)
+    assert_only_a_blinker_is_present(soup)
+    assert_blinker_is_vertical(soup)
 
 def test_blinker_cycles_back_in_any_odd_generation(page):
-    """Test that blinker example page evolves to the next generation"""
-    width, height = 8, 6
     soup = page('/examples/blinker?generation=7')
-    assert len(soup.select('td')) == width * height, f"Expecting a {width}x{height} table"
-    assert len(soup.select('td.cell--alive')) == 3, "Expecting exactly 3 living cells"
+    assert_table_is_displayed(soup)
+    assert_only_a_blinker_is_present(soup)
+    assert_blinker_is_horizontal(soup)
     
-    alive_y_coords = [int(cell['data-y']) for cell in soup.select('td.cell--alive')]
-    assert len(set(alive_y_coords)) == 1, "Expecting all cells to share the same y coordinate"
 
