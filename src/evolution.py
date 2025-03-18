616680b814075fec781aa37e30b8ac435a4eab73
Giorgio Sironi <work@giorgiosironi.com>
2025-03-18 13:51:43 +0000

    Keep alive cells with 3 alive neighbors

--- src/evolution.py
+++ src/evolution.py
@@ -6,6 +6,6 @@ def evolve(world_state: WorldState) -> WorldState:
     for cell in world_state:
         neighbors = find_neighbors(cell)
         alive_neighbors = sum(1 for neighbor in neighbors if neighbor in world_state)
-        if alive_neighbors == 2:
+        if alive_neighbors == 2 or alive_neighbors == 3:
             next_state.append(cell)
     return next_state
