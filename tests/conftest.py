41322e229a9ff40ec4a76ce00801be1a6e057b35
Giorgio Sironi <work@giorgiosironi.com>
2025-03-18 16:29:29 +0000

    Add type annotations to test fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,19 @@
+from flask.testing import FlaskClient
 import pytest
+from typing import TypeAlias, Callable
 from bs4 import BeautifulSoup
 from app import app
 
 @pytest.fixture
-def client():
+def client() -> FlaskClient:
     app.config['TESTING'] = True
     return app.test_client()
 
+PageGetter: TypeAlias = Callable[[str], BeautifulSoup]
+
 @pytest.fixture
-def page(client):
-    def _get_page(url):
+def page(client: FlaskClient) -> PageGetter:
+    def _get_page(url: str) -> BeautifulSoup:
         response = client.get(url)
         return BeautifulSoup(response.data, 'html.parser')
     return _get_page 
